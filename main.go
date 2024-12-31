0b83128d65c95b15dff91d408b8e9ed6e003f8a4
FinnH <finnlharrison@gmail.com>
2024-12-30 23:37:43 -0800

    rgb and button input

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"machine" ///grandcentral-m4"
+	"machine" // grandcentral-m4"
 	"time"
 	// "tinygo.org/x/drivers/ws2812"
 )
 
 func main() {
 	// n := ws2812.New(machine.PC24)
+
+	// probably best to init serial for debugging if you need it
+	machine.InitSerial()
+
+	// wait a half second so serial can sync
+	time.Sleep(time.Millisecond * 500)
 
 	// Blink yellow board LED
 	led := machine.PC30
@@ -21,25 +27,26 @@ func main() {
 			time.Sleep(time.Millisecond * 250)
 		}
 	}()
+	// Configure the pins
+	buttonLedB := machine.PC17
+	buttonLedB.Configure(machine.PinConfig{Mode: machine.PinTimer})
 
-	// Configure the pin
-	buttonB := machine.PC17
-	buttonB.Configure(machine.PinConfig{Mode: machine.PinTimer})
+	buttonLedR := machine.PC16
+	buttonLedR.Configure(machine.PinConfig{Mode: machine.PinTimer})
 
-	buttonR := machine.PC16
-	buttonR.Configure(machine.PinConfig{Mode: machine.PinTimer})
-
+	buttonInput := machine.PB13
+	buttonInput.Configure(machine.PinConfig{Mode: machine.PinInput})
 	// Set up PWM timer
 	pwmTimer := machine.TCC0
 	pwmTimer.Configure(machine.PWMConfig{})
 
-	chB, err := pwmTimer.Channel(buttonB)
+	chB, err := pwmTimer.Channel(buttonLedB)
 	if err != nil {
 		println("Failed to get PWM channel:", err)
 		return
 	}
 
-	chR, err := pwmTimer.Channel(buttonR)
+	chR, err := pwmTimer.Channel(buttonLedR)
 	if err != nil {
 		println("Failed to get PWM channel:", err)
 		return
@@ -68,6 +75,12 @@ func main() {
 			if i <= 0 {
 				i = 0
 				direction = 1
+			}
+			if buttonInput.Get() == true {
+				pwmTimer.Set(chR, 0)
+			} else {
+				pwmTimer.Set(chR, max)
+
 			}
 			pwmTimer.Set(chB, i)
 			time.Sleep(time.Millisecond * 25)
